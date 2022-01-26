8c3b03c4b8a67be09e294ed824c90f67221fe656
Faith Alandi <faefaith@gmail.com>
2022-01-25 16:15:06 -0800

    second commit

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var port = 80;
+
 
 var app = express();
 
@@ -41,7 +44,7 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 //listen on port 80
-var usedPort = process.env.PORT || 80;
-server.listen(usedPort, () => {
+var usedPort = process.env.PORT || port;
+app.listen(usedPort, () => {
 console.log("Server started and listening on port " + usedPort + ".");
 });
